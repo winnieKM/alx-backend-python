90d9629de2e57dfb5dd592fb9e817909d41dcb94
winnieKM <winniemorris254@example.com>
2025-05-16 22:34:08 +0300

    Initial project setup with 0-prime_generator.py

---
+++ python-generators-0x00/prime_generator.py
@@ -0,0 +1,12 @@
+def prime_generator():
+    """Generator function that yields prime numbers indefinitely."""
+    num = 2
+    while True:
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            yield num
+        num += 1
