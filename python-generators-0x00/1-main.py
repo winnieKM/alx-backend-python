dad1c2cfcb83e19997b660a24d176f60321398cd
winnieKM <winniemorris254@example.com>
2025-05-26 10:05:15 +0300

    chore: add .gitignore and remove node_modules from tracking

--- python-generators-0x00/1-main.py
+++
@@ -1,5 +0,0 @@
-from itertools import islice
-stream_users = __import__('0-stream_users')
-
-for user in islice(stream_users.stream_users(), 6):
-    print(user)
--- python-generators-0x00/test_prime.py
+++
@@ -1,8 +0,0 @@
-from prime_generator import prime_generator
-
-gen = prime_generator()
-
-# Print the first 10 prime numbers
-for _ in range(10):
-    print(next(gen))
-
