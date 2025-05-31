6909059d30aff3791260d8fdf4b32a5883b3d82b
winnieKM <winniemorris254@example.com>
2025-05-31 15:14:33 +0300

    Fix access_nested_map to raise KeyError with missing key

--- utils.py
+++ utils.py
@@ -1,18 +1,7 @@
-import requests
-
 def access_nested_map(nested_map, path):
-    """
-    Access a nested map using the provided path.
-    Raise KeyError if the key is not found.
-    """
     for key in path:
-        nested_map = nested_map[key]
+        try:
+            nested_map = nested_map[key]
+        except (TypeError, KeyError):
+            raise KeyError(key)
     return nested_map
-
-
-def get_json(url):
-    """
-    Send a GET request to the given URL and return the JSON response.
-    """
-    response = requests.get(url)
-    return response.json()
