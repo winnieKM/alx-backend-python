6c3710aadb4067c0fb7c3959c475a43cf25eddc1
winnieKM <winniemorris254@example.com>
2025-05-31 18:39:54 +0300

    Restore messaging_app as a normal folder

---
+++ messaging_app/chats/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
