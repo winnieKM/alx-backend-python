9ca9f9647329354dceaa28669e7b2e9e885db343
winniekathomi <kathomimemba@gmail.com>
2025-06-12 12:30:04 +0300

    Add recursive threaded conversation view

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,32 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import cache_page
 from .models import Message
-from django.db.models import Prefetch
+from .managers import MessageManager
+from django.contrib.auth.models import User
 
-@cache_page(60)
 @login_required
+def delete_user(request):
+    """View to delete user account"""
+    user = request.user
+    user.delete()
+    return redirect('login')
+
+@login_required
+@cache_page(60)
 def inbox(request):
-    messages = (
-        Message.objects
-        .filter(receiver=request.user)
-        .select_related('sender', 'receiver', 'parent_message')
-        .prefetch_related(
-            Prefetch('replies', queryset=Message.objects.select_related('sender'))
-        )
-        .only('id', 'sender__username', 'receiver__username', 'content', 'timestamp', 'read')
-        .order_by('-timestamp')
-    )
-    return render(request, 'messaging/inbox.html', {'messages': messages})
+    """Inbox showing unread messages using custom manager, optimized with only()"""
+    unread_messages = Message.unread.unread_for_user(request.user).select_related('sender').only('sender__username', 'content', 'timestamp')
+    return render(request, 'messaging/inbox.html', {'unread_messages': unread_messages})
 
-@cache_page(60)
 @login_required
-def sent_messages(request):
-    messages = (
-        Message.objects
-        .filter(sender=request.user)
-        .select_related('receiver')
-        .only('id', 'receiver__username', 'content', 'timestamp', 'read')
-        .order_by('-timestamp')
-    )
-    return render(request, 'messaging/sent.html', {'messages': messages})
+@cache_page(60)
+def conversation_view(request):
+    """Example conversation view optimized with select_related and prefetch_related"""
+    messages = Message.objects.filter(sender=request.user).select_related('sender').prefetch_related('replies')
+    return render(request, 'messaging/conversation.html', {'messages': messages})
+
+def get_message_thread(message):
+    """Recursively fetch all replies to a message"""
+    thread = []
+    replies = Message.objects.filter(parent_message=message).select_related('sender')
+    for reply in replies:
+        thread.append(reply)
+        thread.extend(get_message_thread(reply))  # recursion
+    return thread
+
+@login_required
+@cache_page(60)
+def threaded_conversation_view(request, message_id):
+    """Display threaded messages starting from a root"""
+    root_message = get_object_or_404(Message, id=message_id)
+    replies = get_message_thread(root_message)
+    return render(request, 'messaging/thread.html', {
+        'root': root_message,
+        'replies': replies,
+    })
